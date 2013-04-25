ee3906711670c5bc6a5e6490a75868a44cbc7e48
Weiyan Shao <lighteningman@gmail.com>
2013-04-25 22:47:31 +0200

    add password as a parameter

--- js/doubanfm.js
+++ js/doubanfm.js
@@ -5,6 +5,6 @@ var tip = "douban.fm";
 exports.html = '<input id="username" type="text"><br><input id="password" type="password"><div class="tip">'+ tip +'</div><text id="error"></text><br/><button type="button" id="import">IMPORT PLAYLIST</button>';
 exports.importData = importData;
 
-function importData(username) {
+function importData(username, password) {
 
 }
